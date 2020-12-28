973ecd17da057797197f0dd91859fa6f7adb448c
Mohamed Ibrahim <32494528+mohamedco96@users.noreply.github.com>
2020-12-28 12:46:05 +0200

    Update module1.blade.php

--- resources/views/module1.blade.php
+++ resources/views/module1.blade.php
@@ -25,34 +25,77 @@
             </li>
         </ul>
         <div class="tab-content" id="pills-tabContent" style="margin-top: 30px">
-            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"> 
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
                 <ol>
-                    <li style="line-height: 2;"><strong><span style="color: rgb(0, 0, 0); font-family: Impact, Charcoal, sans-serif;">يذكر مفهوم الفيديو الرقمي التفاعلي .</span></strong></li>
-                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span style="color: rgb(0, 0, 0);"><strong>يعدد مميزات الفيديو الرقمي التفاعلي .</strong></span></span></li>
-                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span style="color: rgb(0, 0, 0);"><strong>يقارن بين الفيديو الرقمي والفيديو الرقمي التفاعلي .</strong></span></span></li>
-                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span style="color: rgb(0, 0, 0);"><strong>يحدد أسباب استخدام الفيديو الرقمي التفاعلي في العملية التعليمية .</strong></span></span></li>
-                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span style="color: rgb(0, 0, 0);"><strong>يحدد مكونات الفيديو الرقمي التفاعلي .</strong></span></span></li>
-                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span style="color: rgb(0, 0, 0);"><strong>يعدد مواصفات الفيديو الرقمي التفاعلي الفعال .</strong></span></span></li>
-                    <li style="line-height: 2;"><strong><span style="color: rgb(0, 0, 0); font-family: Impact, Charcoal, sans-serif;">يشرح معايير تصميم الفيديو الرقمي التفاعلي .</span></strong></li>
+                    <li style="line-height: 2;"><strong><span
+                                style="color: rgb(0, 0, 0); font-family: Impact, Charcoal, sans-serif;">يذكر مفهوم الفيديو
+                                الرقمي التفاعلي .</span></strong></li>
+                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span
+                                style="color: rgb(0, 0, 0);"><strong>يعدد مميزات الفيديو الرقمي التفاعلي
+                                    .</strong></span></span></li>
+                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span
+                                style="color: rgb(0, 0, 0);"><strong>يقارن بين الفيديو الرقمي والفيديو الرقمي التفاعلي
+                                    .</strong></span></span></li>
+                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span
+                                style="color: rgb(0, 0, 0);"><strong>يحدد أسباب استخدام الفيديو الرقمي التفاعلي في العملية
+                                    التعليمية .</strong></span></span></li>
+                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span
+                                style="color: rgb(0, 0, 0);"><strong>يحدد مكونات الفيديو الرقمي التفاعلي
+                                    .</strong></span></span></li>
+                    <li style="line-height: 2;"><span style="font-family: Impact, Charcoal, sans-serif;"><span
+                                style="color: rgb(0, 0, 0);"><strong>يعدد مواصفات الفيديو الرقمي التفاعلي الفعال
+                                    .</strong></span></span></li>
+                    <li style="line-height: 2;"><strong><span
+                                style="color: rgb(0, 0, 0); font-family: Impact, Charcoal, sans-serif;">يشرح معايير تصميم
+                                الفيديو الرقمي التفاعلي .</span></strong></li>
                 </ol>
-          
+
             </div>
-            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"> لكن لا بد أن
-                أوضح
-                لك أن كل هذه الأفكار المغلوطة حول استنكار النشوة وتمجيد الألم نشأت بالفعل،
-                وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب الشعور
-                بالسعادة،</div>
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                <p style="line-height: 1;"><span style="font-family: Impact, Charcoal, sans-serif; font-size: 18px;">عزيزي
+                        الطالب: عليك إتباع الخطوات التالية لضمان</span></p>
+                <p style="line-height: 1;"><span style="font-size: 18px;"><span
+                            style="font-family: Impact, Charcoal, sans-serif;">دراسة المحتوى التعليمي بصورة
+                            صحيحة:</span></span></p>
+                <p style="line-height: 1;"><span style="font-size: 18px;"><span
+                            style="font-family: Impact, Charcoal, sans-serif;">الإطلاع على الهدف العام لدراسة
+                            الموديول.</span></span></p>
+                <p style="line-height: 1;"><span style="font-size: 18px;"><span
+                            style="font-family: Impact, Charcoal, sans-serif;">الإطلاع على الخطة الزمنية لدراسة
+                            الموديول.</span></span></p>
+                <p style="line-height: 1;"><span style="font-size: 18px;"><span
+                            style="font-family: Impact, Charcoal, sans-serif;">الإطلاع على المحتوى بدقة.</span></span></p>
+                <p style="line-height: 1;"><span style="font-size: 18px;"><span
+                            style="font-family: Impact, Charcoal, sans-serif;">الإجابة على الأسئلة التكوينية
+                            للموديول.</span></span></p>
+                <p style="line-height: 1;"><span style="font-family: Impact, Charcoal, sans-serif; font-size: 18px;">تسليم
+                        النشاط في الموعد المحدد.</span></p>
+                <p><br></p>
+                <p><img src="https://myfiles.space/user_files/73156_373fccd774de6125/73156_custom_files/img1609149976.png">
+                </p>
+                <p><br></p>
+            </div>
             <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab"> لكن لا بد أن
                 أوضح
                 لك أن كل هذه الأفكار المغلوطة حول استنكار النشوة وتمجيد الألم نشأت بالفعل،
                 وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب الشعور
                 بالسعادة،</div>
-            <div class="tab-pane fade" id="pills-contact1" role="tabpanel" aria-labelledby="pills-contact-tab1"> لكن لا بد أن
-                أوضح
-                لك أن كل هذه الأفكار المغلوطة حول استنكار النشوة وتمجيد الألم نشأت بالفعل،
-                وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب الشعور
-                بالسعادة،</div>
-            <div class="tab-pane fade" id="pills-contact2" role="tabpanel" aria-labelledby="pills-contact-tab2"> لكن لا بد أن
+            <div class="tab-pane fade" id="pills-contact1" role="tabpanel" aria-labelledby="pills-contact-tab1">
+                <p style="text-align: center;"><span style="color: rgb(249, 121, 50);"><strong><span
+                                style="font-size: 22px; font-family: Impact, Charcoal, sans-serif;">النشاط
+                                الأول</span></strong></span></p>
+                <p style="text-align: center;"><span style="font-family: Impact, Charcoal, sans-serif;"><span
+                            style="font-size: 28px;">اكتب مقالا قصير عن مفهوم الفيديو الرقمي التفاعلي وأهميته في العملية
+                            التعليمية</span></span></p>
+                <p style="text-align: center;"><span style="font-family: Impact, Charcoal, sans-serif;"><span
+                            style="font-size: 28px;">&nbsp;</span></span></p>
+                <button class="btn btn-lg btn-pill btn-block btn-outline-success  " type="button"
+                    style="border-radius: 22px; width:150px;">
+                    تحميل الكتاب
+                </button>
+            </div>
+            <div class="tab-pane fade" id="pills-contact2" role="tabpanel" aria-labelledby="pills-contact-tab2"> لكن لا بد
+                أن
                 أوضح
                 لك أن كل هذه الأفكار المغلوطة حول استنكار النشوة وتمجيد الألم نشأت بالفعل،
                 وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب الشعور
