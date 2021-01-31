84f1648f2f2265bf38ad896aee345f3faf18a980
dinagwida <74428591+dinagwida@users.noreply.github.com>
2021-01-31 19:44:20 +0200

    change contact us text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -232,7 +232,7 @@
                         <div class="info-box">
                             <i class="bx bx-map"></i>
                             <h3>Our Address</h3>
-                            <p>A108 Adam Street, New York, NY 535022</p>
+                            <p>دمياط- دمياط الجديدة -جامعة دمياط-كلية التربية</p>
                         </div>
                     </div>
 
@@ -240,7 +240,7 @@
                         <div class="info-box">
                             <i class="bx bx-envelope"></i>
                             <h3>Email Us</h3>
-                            <p>info@example.com<br>contact@example.com</p>
+                            <p>dina.gwida@gmail.com</p>
                         </div>
                     </div>
 
@@ -248,7 +248,7 @@
                         <div class="info-box ">
                             <i class="bx bx-phone-call"></i>
                             <h3>Call Us</h3>
-                            <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
+                            <p>+2 01024715454<br>+2 01141921056</p>
                         </div>
                     </div>
 
