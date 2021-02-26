7ccef6f0fa340d7468deede1b3cae14a17490ae7
dinagwida <74428591+dinagwida@users.noreply.github.com>
2021-02-26 21:25:09 +0200

    color

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,7 @@
 <body class="cursors png">
 
     <!-- ======= Header ======= -->
-    <header id="header" style="background-color: #14333f">
+    <header id="header" style="background-color: #34b3e4">
         <div class="container d-flex">
 
             <nav class="nav-menu float-right d-none d-lg-block" >
